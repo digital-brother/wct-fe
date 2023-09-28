15e80eafb38b02c87ffeb8ec85d10d871d55b755
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2023-09-28 12:16:45 +0300

    Add how it works section

---
+++ src/sections/landing/landing-how-it-works.js
@@ -0,0 +1,123 @@
+import Box from '@mui/material/Box';
+import Timeline from '@mui/lab/Timeline';
+import TimelineDot from '@mui/lab/TimelineDot';
+import Container from '@mui/material/Container';
+import TimelineItem from '@mui/lab/TimelineItem';
+import Typography from '@mui/material/Typography';
+import TimelineContent from '@mui/lab/TimelineContent';
+import {alpha, useTheme} from '@mui/material/styles';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+
+import {bgGradient} from 'src/theme/css';
+import {useResponsive} from 'src/hooks/use-responsive';
+import Button from "@mui/material/Button";
+
+// ----------------------------------------------------------------------
+
+const TIMELINES = [
+  {
+    step: 'STEP 1',
+    title: 'Take the health & Wellness Assessment',
+    description:
+      'Tell us about your health goals, conditions, diet and lifestyle',
+  },
+  {
+    step: 'STEP 2',
+    title: 'Get your FREE personalised supplement recommendation',
+    description:
+      'Get your unique plan designed by our leading Functional medicine experts',
+  },
+  {
+    step: 'STEP 3',
+    title: 'Start your journey towards optimal health',
+    description:
+      'Use our app to build healthy habits and maximise your results',
+  },
+];
+
+const COLORS = ['primary', 'secondary', 'warning', 'success'];
+
+// ----------------------------------------------------------------------
+
+export default function LandingHowItWorks() {
+  const theme = useTheme();
+
+  const mdUp = useResponsive('up', 'md');
+
+  return (
+    <Box
+      sx={{
+        ...bgGradient({
+          color: alpha(theme.palette.grey[900], 0.8),
+          imgUrl: '/assets/background/overlay_2.jpg',
+        }),
+        color: 'common.white',
+        py: {xs: 10, md: 15},
+      }}
+    >
+      <Container>
+        <Typography variant="h2" sx={{textAlign: 'center'}}>
+          How It Works
+        </Typography>
+
+        <Typography
+          sx={{
+            mt: 3,
+            mx: 'auto',
+            opacity: 0.72,
+            maxWidth: 480,
+            textAlign: 'center',
+            mb: {xs: 8, md: 10},
+          }}
+        >
+          Nunc nonummy metus. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis.
+        </Typography>
+
+        <Timeline position={mdUp ? 'alternate' : 'right'}>
+          {TIMELINES.map((value, index) => (
+            <TimelineItem
+              key={value.title}
+              sx={{
+                '&:before': {
+                  ...(!mdUp && {display: 'none'}),
+                },
+              }}
+            >
+              <TimelineSeparator>
+                <TimelineDot color={COLORS[index]}/>
+                <TimelineConnector/>
+              </TimelineSeparator>
+
+              <TimelineContent sx={{pb: {xs: 3, md: 5}}}>
+                <Typography variant="overline" sx={{color: `${COLORS[index]}.main`}}>
+                  {value.step}
+                </Typography>
+
+                <Typography variant="h4" sx={{mt: 0.5, mb: 1}}>
+                  {value.title}
+                </Typography>
+
+                <Typography
+                  variant="body2"
+                  sx={{
+                    opacity: 0.72,
+                    maxWidth: {md: 360},
+                    ...(index % 2 && {
+                      ml: 'auto',
+                    }),
+                  }}
+                >
+                  {value.description}
+                </Typography>
+              </TimelineContent>
+            </TimelineItem>
+          ))}
+        </Timeline>
+        <Box sx={{ display: 'flex', justifyContent: 'center', mt: 10}}>
+          <Button variant="contained" color="primary" size="large" >Take the quiz</Button>
+        </Box>
+      </Container>
+    </Box>
+  );
+}
--- src/sections/landing/landing-view.js
+++ src/sections/landing/landing-view.js
@@ -4,6 +4,7 @@
 import LandingTitle from "./landing-title";
 import LandingBenefits from "./landing-benefits";
 import LandingStatistics from "./landing-statistics";
+import LandingHowItWorks from "./landing-how-it-works";
 
 export default function LandingView() {
   return (
@@ -11,6 +12,7 @@ export default function LandingView() {
       <LandingTitle/>
       <LandingStatistics/>
       <LandingBenefits/>
+      <LandingHowItWorks/>
     </>
   );
 }
