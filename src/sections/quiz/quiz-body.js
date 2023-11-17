2f6006b864bc9ec7f652088518fad5dd06092e9c
Slava Loievskyi <loievskyi.slava@gmail.com>
2023-11-17 20:38:50 +0200

    Quiz request sending

--- src/sections/quiz/quiz-body.js
+++ src/sections/quiz/quiz-body.js
@@ -3,18 +3,19 @@ import {SvgIcon} from "@mui/material";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Logo from "../../components/svg-color/Logo.svg";
-import {Lora} from '@next/font/google';
-import Grid from '@mui/material/Grid';
-import useSWR from 'swr'
-import { useState } from 'react';
+import {Lora} from "@next/font/google";
+import Grid from "@mui/material/Grid";
+import useSWR from "swr"
+import { useState } from "react";
 import { useSWRConfig } from "swr"
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
+const post_fetcher = (...args) => fetch(...args).then(res => res.json())
 const lora = Lora({preload: false});
 
 
 function QuestionList() {
-  const { data, error, isLoading } = useSWR('http://localhost:8000/api/v1/quiz/questions/', fetcher);
+  const { data, error, isLoading } = useSWR("http://localhost:8000/api/v1/quiz/questions/", fetcher);
 
   if (error) return <div>Data loading error</div>
   if (isLoading) return <div>Loading...</div>
@@ -23,10 +24,10 @@ function QuestionList() {
 
 function CreateQuiz(email) {
   fetch(
-    'http://localhost:8000/api/v1/quiz/',
+    "http://localhost:8000/api/v1/quiz/",
     {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         email: email
       })
@@ -34,10 +35,37 @@ function CreateQuiz(email) {
   )
 }
 
-function ChoiceVariantButton({ choice }) {
+function CreateAnswer(answer, question) {
+  if (question?.type == "radio") {
+    const response = fetch(
+      "http://localhost:8000/api/v1/quiz/answers/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "BearerToken",
+        },
+        body: JSON.stringify({
+          question: question?.id,
+          choice: answer?.id,
+        })
+      }
+    ).then(response => {
+        if(response.status >= 400) {
+          throw new Error("Server responds with error! " + response.status);
+        } else if(response.status == 201) {
+          alert("Question answered successful");
+          return response.json();
+        }
+    })
+  }
+}
+
+
+function ChoiceVariantButton({ choice, question }) {
 
   function handleClick() {
-    CreateQuiz("loievskyi.slava@gmail.com")
+    CreateAnswer(choice, question)
   }
 
   return (
@@ -45,7 +73,7 @@ function ChoiceVariantButton({ choice }) {
       component="div"
       onClick={handleClick}
       sx={{
-        backgroundColor: '#fbf5ef',
+        backgroundColor: "#fbf5ef",
         padding: "25px",
         borderRadius: "5px",
         margin: "5px",
@@ -62,7 +90,7 @@ function QuestionForm({ question }) {
       <Box
         component="div"
         sx={{
-          alignItems: 'center',
+          alignItems: "center",
           fontWeight: "bold",
           padding: "30px",
           textAlign: "center",
@@ -71,7 +99,7 @@ function QuestionForm({ question }) {
       <Grid container spacing={2} >
         {question?.choices.map((x, i) =>
           <Grid container item xs={6} direction="column" >
-            <ChoiceVariantButton choice={ x } />
+            <ChoiceVariantButton choice={ x } question={ question } />
           </Grid>
         )}
       </Grid>
@@ -81,14 +109,13 @@ function QuestionForm({ question }) {
 
 export default function QuizBody() {
   let questions = QuestionList() || [];
-  console.log(questions[0]);
   return (
     <Box
       component="main"
       sx={{
-        backgroundColor: '#f5f5f5',
-        padding: '24px',
-        alignItems: 'center',
+        backgroundColor: "#f5f5f5",
+        padding: "24px",
+        alignItems: "center",
       }}
     >
       <Container maxWidth="lg" sx={{
