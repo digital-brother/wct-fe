154d6f45a4c82a3ce65fb34782e6cd5001316dc4
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2023-11-17 20:59:44 +0200

    Refactor body

--- src/app/quiz/body.js
+++ src/app/quiz/body.js
@@ -4,6 +4,7 @@ import Box from "@mui/material/Box";
 import {useTheme} from '@mui/material/styles';
 import Container from "@mui/material/Container";
 import Grid from '@mui/material/Grid';
+import {Typography} from "@mui/material";
 
 function VariantButton() {
   const theme = useTheme()
@@ -13,7 +14,6 @@ function VariantButton() {
         backgroundColor: theme.palette.quiz.button.backgroundColor,
         p: 3,
         borderRadius: 0.6,
-        margin: 0.6,
         textAlign: "center",
       }}
     >Variant 1</Box>
@@ -33,32 +33,25 @@ export default function Body() {
       <Container maxWidth="lg" sx={{
         backgroundColor: theme.palette.quiz.container.backgroundColor,
         borderRadius: 2,
-        border: "solid " + theme.palette.quiz.container.borderColor + " 0.2em",
+        border: `solid ${theme.palette.quiz.container.borderColor} 0.2em`,
         pt: 2.6,
         px: 3.2,
         pb: 5,
       }}>
-        <Box
-          sx={{
-            fontWeight: "bold",
-            p: 4,
-            textAlign: "center",
-          }}
-        >QuestionText</Box>
+        <Typography variant="h4" sx={{
+          fontWeight: "bold",
+          p: 4,
+          textAlign: "center",
+        }}
+        >QuestionText
+        </Typography>
         <Box component="form">
-          <Grid container spacing={2} >
-            <Grid container item xs={6} direction="column" >
-              <VariantButton />
-              <VariantButton />
-              <VariantButton />
-              <VariantButton />
-            </Grid>
-            <Grid container item xs={6} direction="column" >
-              <VariantButton />
-              <VariantButton />
-              <VariantButton />
-              <VariantButton />
-            </Grid>
+          <Grid container columnSpacing={4} rowSpacing={1.2}>
+            {[...Array(8)].map(item => (
+              <Grid item xs={6}>
+                <VariantButton/>
+              </Grid>
+            ))}
           </Grid>
         </Box>
       </Container>
