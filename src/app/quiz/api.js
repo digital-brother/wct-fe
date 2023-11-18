d6ef2ce888e87a428a599103399f427d20f251ed
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2023-11-19 00:15:09 +0200

    Refactor axios to API_HOST by default

--- src/app/quiz/api.js
+++ src/app/quiz/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
 const API_HOST = 'http://localhost:8000/api/v1';
-const QUESTIONS_URL = `${API_HOST}/quiz/questions`
+const api = axios.create({baseURL: API_HOST});
+
+const QUESTIONS_PATH = '/quiz/questions';
 
 export async function getQuestions() {
-  const response = await axios.get(QUESTIONS_URL);
+  const response = await api.get(QUESTIONS_PATH);
   return response.data
 }
