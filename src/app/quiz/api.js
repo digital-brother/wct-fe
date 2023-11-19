56222908b7b6215c00d606d7c729c0f814e255ed
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2023-11-19 02:36:04 +0200

    Get questions in body component.

--- src/app/quiz/api.js
+++ src/app/quiz/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const API_HOST = 'http://localhost:8000/api/v1';
-const QUESTIONS_PATH = '/quiz/questions';
+export const QUESTIONS_PATH = '/quiz/questions';
 
 
 export async function fetcher (path){
-  const url = `${API_HOST}/${path}`
+  const url = `${API_HOST}${path}`
   const response = await axios.get(url);
   return response.data;
 }
--- src/app/quiz/body.js
+++ src/app/quiz/body.js
@@ -2,9 +2,12 @@
 
 import Box from "@mui/material/Box";
 import {useTheme} from '@mui/material/styles';
+import useSWR from 'swr'
 import Container from "@mui/material/Container";
 import Grid from '@mui/material/Grid';
 import {Typography} from "@mui/material";
+import {useQuery, useQueryClient} from "@tanstack/react-query";
+import {fetcher, QUESTIONS_PATH} from "@/app/quiz/api";
 
 function VariantButton() {
   const theme = useTheme()
@@ -22,39 +25,47 @@ function VariantButton() {
 
 export default function Body() {
   const theme = useTheme()
-  return (
-    <Box
-      component="main"
-      sx={{
-        backgroundColor: theme.palette.quiz.backgroundColor,
-        p: 3,
-      }}
-    >
-      <Container maxWidth="lg" sx={{
-        backgroundColor: theme.palette.quiz.container.backgroundColor,
-        borderRadius: 2,
-        border: `solid ${theme.palette.quiz.container.borderColor} 0.2em`,
-        pt: 2.6,
-        px: 3.2,
-        pb: 5,
-      }}>
-        <Typography variant="h4" sx={{
-          fontWeight: "bold",
-          p: 4,
-          textAlign: "center",
-        }}
-        >QuestionText
-        </Typography>
-        <Box component="form">
-          <Grid container columnSpacing={4} rowSpacing={1.2}>
-            {[...Array(8)].map((item, index) => (
-              <Grid item xs={6} key={index}>
-                <VariantButton/>
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-      </Container>
-    </Box>
-  );
+  const {data, error, isLoading} = useSWR(QUESTIONS_PATH, fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+  console.log(data)
+  return <div>{JSON.stringify(data)}</div>
 }
+
+//   return (
+//     <Box
+//       component="main"
+//       sx={{
+//         backgroundColor: theme.palette.quiz.backgroundColor,
+//         p: 3,
+//       }}
+//     >
+//       <Container maxWidth="lg" sx={{
+//         backgroundColor: theme.palette.quiz.container.backgroundColor,
+//         borderRadius: 2,
+//         border: `solid ${theme.palette.quiz.container.borderColor} 0.2em`,
+//         pt: 2.6,
+//         px: 3.2,
+//         pb: 5,
+//       }}>
+//         <Typography variant="h4" sx={{
+//           fontWeight: "bold",
+//           p: 4,
+//           textAlign: "center",
+//         }}
+//         >QuestionText
+//         </Typography>
+//         <Box component="form">
+//           <Grid container columnSpacing={4} rowSpacing={1.2}>
+//             {[...Array(8)].map((item, index) => (
+//               <Grid item xs={6} key={index}>
+//                 <VariantButton/>
+//               </Grid>
+//             ))}
+//           </Grid>
+//         </Box>
+//       </Container>
+//     </Box>
+//   );
+// }
