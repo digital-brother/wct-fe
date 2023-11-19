10613247b56cbdc0b2f0df3a547f4ec318d165e6
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2023-11-19 02:14:39 +0200

    Rewrite api.js to fit SWR

--- src/app/quiz/api.js
+++ src/app/quiz/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const API_HOST = 'http://localhost:8000/api/v1';
-const api = axios.create({baseURL: API_HOST});
-
 const QUESTIONS_PATH = '/quiz/questions';
 
-export async function getQuestions() {
-  const response = await api.get(QUESTIONS_PATH);
-  return response.data
+
+export async function fetcher (path){
+  const url = `${API_HOST}/${path}`
+  const response = await axios.get(url);
+  return response.data;
 }
