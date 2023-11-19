ea1f0dea2f7af43308293a5fc7d302943a368415
Shuryhin Oleksandr <shuryhin.oleksandr@gmail.com>
2023-11-19 02:14:13 +0200

    Get rid of react query

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,25 +1,21 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import ThemeRegistry from '@/components/ThemeRegistry/ThemeRegistry';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
 export const metadata = {
   title: 'Nutra Nourish',
   description: 'Nutra Nourish',
 };
 
-const queryClient = new QueryClient()
 
 export default function RootLayout({children}) {
   return (
     <html lang="en">
     <body>
     <ThemeRegistry>
-      <QueryClientProvider client={queryClient}>
         <Box>
           {children}
         </Box>
-      </QueryClientProvider>
     </ThemeRegistry>
     </body>
     </html>
