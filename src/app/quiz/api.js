ab09ddfb2e1e31125c637f59fe264966b63c0068
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-11-22 00:47:43 +0200

    Add postAnswer to api.js

--- src/app/quiz/api.js
+++ src/app/quiz/api.js
@@ -1,11 +1,26 @@
-import axios from 'axios'
+let axios = require('axios')
 
 const API_HOST = 'http://localhost:8000/api/v1';
-export const QUESTIONS_PATH = '/quiz/questions';
+export const QUESTIONS_PATH = '/quiz/questions/';
+export const ANSWERS_PATH = '/quiz/answers/';
 
+const client = axios.create({
+  baseURL: API_HOST,
+  timeout: 1000,
+  headers: {Cookie: 'sessionid=1xt7r64kod93g9kwjtpwv9yl15s73eav'},
+});
 
-export async function fetcher (path){
-  const url = `${API_HOST}${path}`
-  const response = await axios.get(url);
+export async function getQuestions() {
+  const response = await client.get(QUESTIONS_PATH);
   return response.data;
 }
+
+export async function postAnswer(data) {
+  const response = await client.post(ANSWERS_PATH, data)
+  return response.data
+}
+
+
+// const data = {question: 3, text: 47}
+// const createdAnswer = await postAnswer(data)
+// console.log(createdAnswer);
