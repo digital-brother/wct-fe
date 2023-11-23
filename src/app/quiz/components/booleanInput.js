5e20ee44dbcc7f94db6a65986a687009c00f3779
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-11-23 23:29:03 +0200

    Make styling for boolean input

--- src/app/quiz/components/booleanInput.js
+++ src/app/quiz/components/booleanInput.js
@@ -1,9 +1,41 @@
 import Box from "@mui/material/Box";
+import {Button} from "@mui/material";
 
-export default function BooleanInput() {
+function BooleanButton({label, mr=0}) {
   return (
-    <Box>
-      Boolean input
+    <Button sx={{
+      width: '172.74px',
+      height: '88px',
+      mr: mr,
+
+      border: '1.09px #B0835F solid',
+      borderRadius: 54.32,
+
+      fontWeight: 500,
+      fontSize: 32.59,
+      fontFamily: 'Fahkwang',
+
+      color: "#121211",
+      bgcolor: "#FFFFFF",
+
+      "&:hover": {
+        bgcolor: "#1F4934",
+        color: "#FFFFFF",
+      },
+
+      textTransform: 'none',
+    }}>
+      {label}
+    </Button>
+  )
+}
+
+export default function BooleanInput({question, ...props}) {
+  console.log(props);
+  return (
+    <Box {...props}>
+      <BooleanButton label="Yes" mr={3}/>
+      <BooleanButton label="No"/>
     </Box>
   )
 }
