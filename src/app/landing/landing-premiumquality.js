c23d15284c0f976f13ecd36f8caa37187d20bf7b
NikGorbunov <gorbunovmya.@gmail.com>
2023-11-30 17:20:56 +0200

    added pages to landing folder

---
+++ src/app/landing/landing-premiumquality.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Logo from "../../../public/images/logo.svg";
+
+export default function LandingPremiumQuality() {
+    return (
+        <Box>
+            <span></span>
+            <Box>
+                <Box>
+                    <Image src={""} alt="Logo" width={321} height={64} style={{display: "block"}}/>
+                    <span></span>
+                </Box>
+            </Box>
+        </Box>
+    );
+}
